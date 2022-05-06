225dc5be51573e4e9ddd49c334c1b877c8e1e03c
Ayannezirova <105051466+Ayannezirova@users.noreply.github.com>
2022-05-06 15:22:55 +0400

    calisma1 hazirdir

---
+++ codes/java-se/java1/src/giris1/calisma1.java
@@ -0,0 +1,9 @@
+package giris1;
+
+public class calisma1 {
+
+	public static void main(String[] args) {
+		  System.out.print("Ayan");
+	}
+
+}
---
+++ codes/java-se/java1/src/giris1/giris.java
@@ -0,0 +1,9 @@
+package giris1;
+
+public class giris {
+
+	public static void main(String[] args) {
+      System.out.print("Salam");
+	}
+
+}
