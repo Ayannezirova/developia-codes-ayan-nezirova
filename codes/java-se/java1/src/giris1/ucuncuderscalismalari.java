86370149f24605cb1243e814c0d9f6c3e4c248fe
Ayannezirova <105051466+Ayannezirova@users.noreply.github.com>
2022-05-14 18:45:30 +0400

    3-cü dərs çalışmaları. Misal 2.

---
+++ codes/java-se/java1/src/giris1/ucuncuderscalismalari.java
@@ -0,0 +1,22 @@
+package giris1;
+
+public class ucuncuderscalismalari {
+
+	public static void main(String[] args) {
+		
+			
+	    for (int i = 2; i <= 113; i++) {
+	    if (i % 2 == 1) {
+	        System.out.println(i);
+	    }
+ 
+	    }
+		
+		 }
+		 
+			
+		}
+		
+	
+
+
