c303c5abc3251a94a0a3cef50c1c523911c53ad9
Ayannezirova <105051466+Ayannezirova@users.noreply.github.com>
2022-05-15 11:59:32 +0400

    3-cü dərs çalışmaları. Çalışma 1

---
+++ codes/java-se/java1/src/giris1/ucuncuderscalismalar.java
@@ -0,0 +1,15 @@
+package giris1;
+
+public class ucuncuderscalismalar {
+
+	public static void main(String[] args) {
+		
+		for(int i=65;i<=90;i++) {
+		System.out.println((char)i);	
+			
+		
+		}
+
+	}
+
+}
