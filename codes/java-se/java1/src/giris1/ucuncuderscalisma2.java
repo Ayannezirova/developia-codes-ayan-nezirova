fceec72506fc2c4f69c51d92e65a46213390cea6
Ayannezirova <105051466+Ayannezirova@users.noreply.github.com>
2022-05-15 13:11:24 +0400

    3-cü dərs çalışmaları. Çalışma 2. Müəllimin izahı ilə

---
+++ codes/java-se/java1/src/giris1/ucuncuderscalisma2.java
@@ -0,0 +1,17 @@
+package giris1;
+
+public class ucuncuderscalisma2 {
+
+	public static void main(String[] args) {
+		
+		int eded=9;
+		boolean sade=true; 
+		for (int i = 2; i <= eded-1; i++) {
+	    if (eded % i == 0) {
+		    	sade=false;
+		      
+		    }
+	}
+		  System.out.println(sade);
+}
+}
