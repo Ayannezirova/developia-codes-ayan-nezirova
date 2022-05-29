48e4852f112a396e5783c225afe1b90a66874108
Ayannezirova <105051466+Ayannezirova@users.noreply.github.com>
2022-05-29 11:55:54 +0400

    String tipində massivlər/dərs 7/çalışma 2

---
+++ codes/java-se/java1/src/massivCalismalar/StringMassiv.java
@@ -0,0 +1,17 @@
+package massivCalismalar;
+
+public class StringMassiv {
+
+	public static void main(String[] args) {
+		String[] names = new String[5];
+		names[0] = "Aydan";
+		names[1] = "Kenan";
+		names[2] = "Ramil";
+		names[3] = "Nermin";
+		names[4] = "Fidan";
+
+	
+	for (int i = 0; i < 5; i++)
+		System.out.println(names[i]);
+}
+}
