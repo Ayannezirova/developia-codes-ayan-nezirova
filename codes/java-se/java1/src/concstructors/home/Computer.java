641caed6129799a778f8974b52af5608417a2e8f
Ayannezirova <105051466+Ayannezirova@users.noreply.github.com>
2022-06-03 11:22:33 +0400

    ders 9/metodlar

---
+++ codes/java-se/java1/src/concstructors/home/Computer.java
@@ -0,0 +1,13 @@
+package concstructors.home;
+
+public class Computer {
+
+	Integer sum;
+	String brand;
+	String model;
+	String color;
+
+	public void arraySum(int i, int j) {
+		System.out.println(i + j);
+	}
+}
