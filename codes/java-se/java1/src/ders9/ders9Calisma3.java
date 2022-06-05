e5681d44ba9c77dfd1ebd3246fb56e6ab6aeccef
Ayannezirova <105051466+Ayannezirova@users.noreply.github.com>
2022-06-05 12:08:14 +0400

    dərs 9/çalışma 3/ikincisi

---
+++ codes/java-se/java1/src/ders9/ders9Calisma3.java
@@ -0,0 +1,16 @@
+package ders9;
+
+public class ders9Calisma3 {
+
+	int sum(int k) {
+		return k*k;
+
+	}
+
+	public static void main(String[] args) {
+		ders9Calisma3 kvadrataQaldirma = new ders9Calisma3();
+		int t = kvadrataQaldirma.sum(15);
+		System.out.println(t);
+	}
+
+}
