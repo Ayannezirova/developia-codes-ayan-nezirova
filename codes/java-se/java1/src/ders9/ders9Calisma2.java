8d5df444a7cc0767f1a11063f952be6d18be4f94
Ayannezirova <105051466+Ayannezirova@users.noreply.github.com>
2022-06-05 12:05:59 +0400

    ders 9/çalışma 2/ikinci

---
+++ codes/java-se/java1/src/ders9/ders9Calisma2.java
@@ -0,0 +1,16 @@
+package ders9;
+
+public class ders9Calisma2 {
+
+	int sum(int i, int j, int k, int l, int m) {
+		return i+j+k+l+m;
+
+	}
+
+	public static void main(String[] args) {
+		ders9Calisma2 edediorta = new ders9Calisma2();
+		int t = edediorta.sum(17, 23, 9, 38, 90)/5;
+		System.out.println(t);
+	}
+
+}
