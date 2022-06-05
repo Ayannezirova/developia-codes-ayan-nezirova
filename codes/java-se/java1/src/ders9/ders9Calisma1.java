47729315e0e98aa828426c92513ff71f70879bac
Ayannezirova <105051466+Ayannezirova@users.noreply.github.com>
2022-06-05 11:58:55 +0400

    ders 9/calışma 1/ikinci

---
+++ codes/java-se/java1/src/ders9/ders9Calisma1.java
@@ -0,0 +1,15 @@
+package ders9;
+
+public class ders9Calisma1 {
+
+	int sum(int i, int j, int k, int l) {
+		return i + j + k + l;
+	}
+
+	public static void main(String[] args) {
+		ders9Calisma1 cemleme = new ders9Calisma1();
+		int t = cemleme.sum(21, 15, 56, 8);
+		System.out.println(t);
+	}
+
+}
