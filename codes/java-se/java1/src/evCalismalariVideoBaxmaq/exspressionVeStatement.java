97a9ad71949327cd7dd462c6f8e37d3f22dcfa13
Ayannezirova <105051466+Ayannezirova@users.noreply.github.com>
2022-06-13 15:55:45 +0400

    Exspression ve statement

---
+++ codes/java-se/java1/src/evCalismalariVideoBaxmaq/exspressionVeStatement.java
@@ -0,0 +1,16 @@
+package evCalismalariVideoBaxmaq;
+
+public class exspressionVeStatement {
+
+	public static void main(String[] args) {
+ 
+		int a = 9;
+		int b = 32;
+		int c = a + b;
+//burada "a+b" exspression, "int c = a + b;" ise statement adlanir
+		System.out.println(a + b + c);
+//burada "a+b+c" exspression, "System.out.println(a + b + c);" ise statement adlanir
+		
+	}
+
+}
