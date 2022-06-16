ccb3fcd7ca1271055038adb119b4dfb5ac4b7410
Ayannezirova <105051466+Ayannezirova@users.noreply.github.com>
2022-06-16 19:37:46 +0400

    Stringi primitiv tiplere cevirme

---
+++ codes/java-se/java1/src/ders12calismalar/tipCevrilmeleri.java
@@ -0,0 +1,17 @@
+package ders12calismalar;
+
+public class tipCevrilmeleri {
+
+	public static void main(String[] args) {
+		
+		String m="563";
+		int a=Integer.valueOf(m);
+		double b=Double.valueOf(m);
+		boolean c=Boolean.valueOf(m);
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+	}
+
+}
