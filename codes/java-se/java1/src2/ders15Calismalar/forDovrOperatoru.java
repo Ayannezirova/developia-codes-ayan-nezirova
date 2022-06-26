a7c16d726027f9e92e955dc006ca29cf0c87f9b6
Ayannezirova <105051466+Ayannezirova@users.noreply.github.com>
2022-06-26 21:36:55 +0400

    For dövr operatoru

---
+++ codes/java-se/java1/src2/ders15Calismalar/forDovrOperatoru.java
@@ -0,0 +1,40 @@
+package ders15Calismalar;
+
+public class forDovrOperatoru {
+	public static void main(String[] args) {
+
+		// vurma cədvəlini misal götürək.
+
+		for (int i = 1; i <= 10; i++) {
+			System.out.println("1 * " + i + " = " + 1 * i);
+		}
+		System.out.println("_______________________");
+
+		for (int i = 1; i <= 10; i++) {
+			System.out.println("2 * " + i + " = " + 2 * i);
+		}
+		System.out.println("_______________________");
+
+		for (int i = 1; i <= 10; i++) {
+			System.out.println("3 * " + i + " = " + 3 * i);
+		}
+		System.out.println("_______________________");
+
+		for (int i = 1; i <= 10; i++) {
+			System.out.println("4 * " + i + " = " + 4 * i);
+		}
+//ve s.
+		/*
+		 * For birinci bölümü işə salır və ikinci bölümə keçir. İkinci bölümdəki şərt
+		 * true olarsa, üçüncü bölümə keçmədən forun içini{} edir. Birinci bölüm sadəcə
+		 * birinci dövrdə işə düşür. Birinci dövr xaricində digər bütün dövrlərdə üçüncü
+		 * bölüm icra olunur.
+		 */
+
+		/*
+		 * * for da birinci bölümün adı initialization(başlamaq) adlanır.
+		 * For da ikinci bölümün adı candition(şərt bölümü) adlanır. 
+		 * Üçüncü bölümün adı isə update(başlanğıca verdiyimiz dəyərin dəyişdirilməsi) adlanır.
+		 */
+	}
+}
