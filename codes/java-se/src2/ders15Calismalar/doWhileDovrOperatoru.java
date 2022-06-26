e84990726e554754ce9d281e264455e96d36e70b
Ayannezirova <105051466+Ayannezirova@users.noreply.github.com>
2022-06-26 23:30:44 +0400

    do while dövr operatoru

---
+++ codes/java-se/src2/ders15Calismalar/doWhileDovrOperatoru.java
@@ -0,0 +1,19 @@
+package ders15Calismalar;
+
+public class doWhileDovrOperatoru {
+
+	public static void main(String[] args) {
+
+		// son şərtli:
+		// while ilə do while fərqi: do while ilk dövrünə şərti yoxlamadan daxil olur.
+
+		int age = 16;
+		do {
+			System.out.println(age);
+			age++;
+
+		} while (age < 20);
+
+	}
+
+}
