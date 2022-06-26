0e7921cf66dbd80bde11268fe7c8ca15178d01d4
Ayannezirova <105051466+Ayannezirova@users.noreply.github.com>
2022-06-26 23:31:59 +0400

    Break istifadəsi

---
+++ codes/java-se/src2/ders15Calismalar/breakIstifadesi.java
@@ -0,0 +1,21 @@
+package ders15Calismalar;
+
+public class breakIstifadesi {
+
+	public static void main(String[] args) {
+
+		for (int i = 1; i <= 10; i++) {
+			System.out.println(i);
+			if (i == 7)
+				break;
+
+			/*
+			 * Break dövrlərdə istifadə olunur. Dövrün bitməsi üçün işlədilir. Yuxarıdakı
+			 * misalda i 7 olduqda şərt ödənir və break girdiyi üçün dövr bitir.
+			 */
+
+
+		}
+	}
+
+}
