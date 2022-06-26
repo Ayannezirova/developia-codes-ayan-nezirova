36824c197e262a212ca0cfe80814a1b1ee35b1ba
Ayannezirova <105051466+Ayannezirova@users.noreply.github.com>
2022-06-26 23:30:20 +0400

    while dövr operatoru

---
+++ codes/java-se/java1/src/bir/ekranacixarma.java
@@ -0,0 +1,17 @@
+package bir;
+
+public class ekranacixarma {
+
+	public static void main(String[] args) {
+		jshell> String name="Ayan";
+		name ==> "Ayan"
+
+		jshell> int age=15;
+		age ==> 15
+
+		jshell> System.out.print(age+" "+name)
+		15 Ayan
+
+	}
+
+}
