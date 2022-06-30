5bd01e7ffdcae249f6f1201ff60198bf03c30d34
Ayannezirova <105051466+Ayannezirova@users.noreply.github.com>
2022-06-30 23:14:02 +0400

    Arraylist

---
+++ codes/java-se/java1/src2/ders16Calismalar/MyArrayList.java
@@ -0,0 +1,49 @@
+package ders16Calismalar;
+
+public class MyArrayList<T> {
+
+	public int size() {
+		return index;
+	}
+
+	Object[] array;
+	int index = 0;
+	int length;
+	int initial;
+
+	public MyArrayList(int length, int initial) {
+		this.length = length;
+		this.initial = initial;
+		array = new Object[length];
+
+	}
+
+	public void add(T j) {
+		array[index] = j;
+		index++;
+		if (index == array.length) {
+			Object[] a = new Object[array.length + length];
+			for (int i = 0; i < array.length; i++) {
+				a[i] = array[i];
+			}
+			array = a;
+		}
+	}
+
+	public String toString() {
+		String s = "(";
+		for (int i = 0; i < index; i++) {
+			if (i == index - 1) {
+				s += array[i];
+
+			} else {
+				s += array[i] + ", ";
+			}
+		}
+		s += ")";
+		return s;
+	}
+
+	
+
+}
