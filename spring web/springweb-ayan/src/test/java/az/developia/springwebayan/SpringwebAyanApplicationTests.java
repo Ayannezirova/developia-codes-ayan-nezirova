2e9301675ecfe38ceb38961005017ae135adcdf7
Ayannezirova <105051466+Ayannezirova@users.noreply.github.com>
2022-08-08 15:35:48 +0400

    spring web proyekti quruldu

---
+++ spring web/springweb-ayan/src/test/java/az/developia/springwebayan/SpringwebAyanApplicationTests.java
@@ -0,0 +1,13 @@
+package az.developia.springwebayan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringwebAyanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
