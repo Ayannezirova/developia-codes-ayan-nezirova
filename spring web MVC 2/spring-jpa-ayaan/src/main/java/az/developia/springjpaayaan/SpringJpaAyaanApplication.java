78ebcf113a86ddf1f307aef927d8efa1eda772fa
Ayannezirova <105051466+Ayannezirova@users.noreply.github.com>
2022-08-28 16:44:49 +0400

    jpa proyekti quruldu

---
+++ spring web MVC 2/spring-jpa-ayaan/src/main/java/az/developia/springjpaayaan/SpringJpaAyaanApplication.java
@@ -0,0 +1,13 @@
+package az.developia.springjpaayaan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaAyaanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJpaAyaanApplication.class, args);
+	}
+
+}
---
+++ spring web MVC 2/spring-jpa-ayaan/src/test/java/az/developia/springjpaayaan/SpringJpaAyaanApplicationTests.java
@@ -0,0 +1,13 @@
+package az.developia.springjpaayaan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaAyaanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
