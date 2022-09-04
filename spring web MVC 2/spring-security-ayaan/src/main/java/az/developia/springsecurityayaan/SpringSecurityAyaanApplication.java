9b512e50abdf071492b74920d79d629dc7e2b793
Ayannezirova <105051466+Ayannezirova@users.noreply.github.com>
2022-09-04 12:13:23 +0400

    spring security proyekti quruldu

---
+++ spring web MVC 2/spring-security-ayaan/src/main/java/az/developia/springsecurityayaan/SpringSecurityAyaanApplication.java
@@ -0,0 +1,13 @@
+package az.developia.springsecurityayaan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityAyaanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityAyaanApplication.class, args);
+	}
+
+}
